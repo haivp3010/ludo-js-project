30a9c318f050fba4e6bc24e0c532d83bb9259bce
Nguyen Duy Hai <nguyenduyhai6e@gmail.com>
2016-04-18 22:29:03 +0700

    Experimenting new template

---
+++ draft/haind/Assets/Scripts/Dice.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour
+{
+    private static int number= 10;
+
+    public int Number
+    {
+        get { return number; }
+        set { number = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ draft/haind/Assets/Scripts/HorseControl.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorseControl : MonoBehaviour
+{
+    public Animator anim;
+    private GameObject clicked;
+    public float speed;
+    public static Dice dice1 = new Dice();
+    private int i = 0;
+    private int iClick = dice1.Number;
+    public int horseNumber; // 0 - 15
+    private int position;
+    private HorseColor color;
+    void Start()
+    {
+        color = GameLogic.GetHorseColor(horseNumber);
+        position = PositionControl.GetStartPosition(color);
+    }
+    void Update()
+    {
+        //gameObject.transform.position = PositionControl.GetRealPosition(position);
+        var step = speed * Time.deltaTime;
+
+        if (clicked != null)
+        {
+            anim.enabled = true;
+            clicked.transform.position = Vector3.MoveTowards(transform.position, PositionControl.GetRealPosition(++position), step);
+
+            if (clicked.transform.position == PositionControl.GetRealPosition(position))
+            {
+                i++;
+                Debug.Log("IClick: " + iClick);
+                if (i > iClick)
+                {
+                    Debug.Log(iClick);
+                    clicked = null;
+
+                    anim.enabled = false;
+                }
+                if (i == 48)
+                {
+                    i = 0;
+                    iClick -= 48;
+                }
+                if (i == 36)
+                {
+                    i = 101;
+                    iClick += 65;
+                }
+
+
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked!");
+        if (clicked == null)
+            Debug.Log("null");
+        clicked = gameObject;
+        i = 0;
+        Debug.Log(clicked.ToString());
+    }
+
+    private void OnMouseEnter()
+    {
+        anim.enabled = true;
+
+        Debug.Log("MouseEnter");
+    }
+
+    private void OnMouseExit()
+    {
+        anim.enabled = false;
+        Debug.Log("MouseExit");
+    }
+}
