b72ec3956a9814b9da055a160608d8408025b41f
nguyenlcse04555 <nguyenlcse04555@fpt.edu.vn>
2016-04-18 16:18:11 +0700

    Added sample project
    The sample project will help you to design your code better, especially
    in game logic. However it's still in development.

---
+++ draft/nguyenlc/SampleLudo/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum HorseColor
+{
+    None = -1,
+    Red = 0,
+    Blue = 1,
+    Yellow = 2,
+    Green = 3
+}
+
+public class PositionControl
+{
+    private const int FirstSpawnPosition = 80;
+    private static SortedList<int, Vector3> RealPositionList = new SortedList<int, Vector3>
+    {
+        { 0, new Vector3(2.93f, 1.21f) },
+        { 1, new Vector3(2.35f, 1.21f) },
+        { 2, new Vector3(1.77f, 1.21f) },
+        { 3, new Vector3(1.19f, 1.21f) }
+    };
+    private static SortedList<HorseColor, int> StartPositionList = new SortedList<HorseColor, int>
+    {
+        { HorseColor.Red, 0 },
+        { HorseColor.Blue, 14 },
+        { HorseColor.Yellow, 28 },
+        { HorseColor.Green, 42 }
+    };
+    private static SortedList<HorseColor, int> FirstCagePositionList = new SortedList<HorseColor, int>
+    {
+        { HorseColor.Red, 56 },
+        { HorseColor.Blue, 62 },
+        { HorseColor.Yellow, 68 },
+        { HorseColor.Green, 74 }
+    };
+    public static Vector3 GetRealPosition(int position)
+    {
+        return RealPositionList[position];
+    }
+    public static int GetStartPosition(HorseColor color)
+    {
+        return StartPositionList[color];
+    }
+    public static int GetCagePosition(HorseColor color, int cageNumber)
+    {
+        return FirstCagePositionList[color] + cageNumber - 1;
+    }
+    public static int GetSpawnPosition(int horseNumber)
+    {
+        return horseNumber + FirstSpawnPosition - 1;
+    }
+
+}
+
+public class GameLogic : MonoBehaviour {
+    public const int NUMBER_OF_PLAYERS = 4; // Red, Blue, Yellow, Green
+    public const int NUMBER_OF_HORSES_PER_PLAYER = 4;
+    public const int NUMBER_OF_HORSES = NUMBER_OF_PLAYERS * NUMBER_OF_HORSES_PER_PLAYER;
+    public static HorseColor GetHorseColor(int horseNumber)
+    {
+        if ((horseNumber >= 0) && (horseNumber < GameLogic.NUMBER_OF_HORSES))
+        {
+            return (HorseColor)(horseNumber / GameLogic.NUMBER_OF_HORSES_PER_PLAYER);
+        }
+        return HorseColor.None;
+    }
+
+    void Awake ()
+    {
+        
+    }
+}
---
+++ draft/nguyenlc/SampleLudo/Assets/Scripts/HorseControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorseControl : MonoBehaviour {
+    public int horseNumber; // 0 - 15
+    private int position;
+    private HorseColor color;
+	void Start() {
+        color = GameLogic.GetHorseColor(horseNumber);
+        position = PositionControl.GetStartPosition(color);
+	}
+	void Update() {
+        gameObject.transform.position = PositionControl.GetRealPosition(position);
+	}
+
+    void OnMouseDown ()
+    {
+        position++;
+    }
+}
