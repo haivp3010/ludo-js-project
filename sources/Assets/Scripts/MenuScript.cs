85d95f6639df367a369f6e8eaeb6dc71afe3895e
Rexx0r <minhh.nguyen.97@gmail.com>
2016-05-12 12:25:46 +0700

    ui select player

--- sources/Assets/Scripts/MenuScript.cs
+++ sources/Assets/Scripts/MenuScript.cs
@@ -5,26 +5,42 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
     
-    public Button startButton;
+    public Button playButton;
     public Button quitButton;
     public Button helpButton;
     public Button backButton;
+    public Button decreaseButton;
+    public Button increaseButton;
     public Canvas helpMenu;
+    public Canvas playMenu;
+    public Text numberPlayer;
+    private int number = 4;
 	// Use this for initialization
 	void Start () {
-        
-        startButton = startButton.GetComponent<Button>();
+        numberPlayer = numberPlayer.GetComponent<Text>();
+        playButton = playButton.GetComponent<Button>();
         quitButton = quitButton.GetComponent<Button>();
         helpButton = helpButton.GetComponent<Button>();
+        decreaseButton = decreaseButton.GetComponent<Button>();
+        increaseButton = increaseButton.GetComponent<Button>();
         helpMenu = helpMenu.GetComponent<Canvas>();
+        playMenu = playMenu.GetComponent<Canvas>();
         helpMenu.enabled = false;
+        playMenu.enabled = false;
+        
+        numberPlayer.text = " " +  number.ToString();
     }
 	
 	// Update is called once per frame
-	public void Play () {
-        SceneManager.LoadScene("ludo");
+	public void PlayMenu () {
+        //SceneManager.LoadScene("ludo");
+        playMenu.enabled = true;
     }
 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ludo");
+    }
     public void Quit ()
     {
         Application.Quit();
@@ -37,5 +53,25 @@ public class MenuScript : MonoBehaviour {
     public void Back()
     {
         helpMenu.enabled = false;
+    }
+    public void Decrease()
+    {
+        if (number > 2 && number <=4)
+        {
+            number--;
+            decreaseButton.enabled = true;
+        }
+        //else decreaseButton.enabled = false;
+        numberPlayer.text = " " + number.ToString();
+    }
+    public void Increase()
+    {
+        if (number < 4 && number >=2)
+        {
+            number++;
+            increaseButton.enabled = true;
+        }
+        //else increaseButton.enabled = false;
+        numberPlayer.text = " " + number.ToString();
     }
 }
