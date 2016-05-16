3f61549cad6a63bbc1d34dbe272fccd9952f888a
Rexx0r <minhh.nguyen.97@gmail.com>
2016-05-16 22:09:33 +0700

    xabi alonso

--- sources/Assets/Scripts/Winner.cs
+++ sources/Assets/Scripts/Winner.cs
@@ -6,25 +6,54 @@ using UnityEngine.SceneManagement;
 public class Winner : MonoBehaviour {
     public Button exitGame;
     public Button resetGame;
-    public Text winner;
+    //public Text winner;
     public Canvas gameOver;
+    public Animator anim;
+    public SpriteRenderer winner;
 	// Use this for initialization
 	void Start () {
         exitGame = exitGame.GetComponent<Button>();
         resetGame = resetGame.GetComponent<Button>();
-        winner = winner.GetComponent<Text>();
+        //winner = winner.GetComponent<Text>();
         gameOver = gameOver.GetComponent<Canvas>();
+        anim = anim.GetComponent<Animator>();
+        winner = winner.GetComponent<SpriteRenderer>();
         gameOver.enabled = false;
-	}
+        anim.enabled = false;
+        winner.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (GameState.Instance.Winner != HorseColor.None)
+        if (GameState.Instance.Winner == HorseColor.Red)
         {
-            winner.text = GameState.Instance.Winner + " Wins!";
             gameOver.enabled = true;
+            winner.enabled = true;
+            anim.enabled = true;
+            anim.Play("red_win_animation");
         }
-	}
+        else if (GameState.Instance.Winner == HorseColor.Green)
+        {
+            gameOver.enabled = true;
+            winner.enabled = true;
+            anim.enabled = true;
+            anim.Play("green_win_animation");
+        }
+        else if (GameState.Instance.Winner == HorseColor.Blue)
+        {
+            gameOver.enabled = true;
+            winner.enabled = true;
+            anim.enabled = true;
+            anim.Play("blue_win_animation");
+        }
+        else if (GameState.Instance.Winner == HorseColor.Yellow)
+        {
+            gameOver.enabled = true;
+            winner.enabled = true;
+            anim.enabled = true;
+            anim.Play("yellow_win_animation");
+        }
+    }
 
     public void Exit()
     {
